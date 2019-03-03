845a027454d7c3e8c46e8a689f2e2c5cdc333462
Jeff Hiltz <jeffhiltz@gmail.com>
2019-03-03 16:20:23 +0100

    add boilerplate awesome autostart.sh

---
+++ awesome/.config/awesome/autostart.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# ---
+# Use "run program" to run it only if it is not already running
+# Use "program &" to run it regardless
+# ---
+# NOTE: This script runs with every restart of AwesomeWM
+
+function run {
+  if ! pgrep $1 > /dev/null;
+  then
+    $@&
+  fi
+}
+
