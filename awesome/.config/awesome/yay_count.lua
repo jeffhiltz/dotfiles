b26d25558218b3f996032bc40a536ac2173a6e81
Jeff Hiltz <jeffhiltz@gmail.com>
2019-05-15 22:03:44 +0200

    improve yay count widget

--- awesome/.config/awesome/yay_count.lua
+++ awesome/.config/awesome/yay_count.lua
@@ -1,4 +1,5 @@
 -- count available package updates (using yay and pacman)
+-- NOTE: need to be able to run "sudo pacman -Sy" without a password.
 
 local awful = require("awful")
 local beautiful = require("beautiful")
@@ -9,16 +10,27 @@ local update_interval = 3600 -- in seconds
 local yay_count_widget = wibox.widget.textbox()
 
 local function update_widget(count)
-  local colour = count > 0 and beautiful.xcolor3 or beautiful.xcolor7
-  yay_count_widget.markup = "<span foreground='" .. colour .. "'>" .. count .. "</span>"
+  local colour
+  local text
+  if count < 0 then
+    colour = beautiful.xcolor9
+    text = "Err"
+  elseif count == 0 then
+    colour = beautiful.xcolor7
+    text = "No Updates"
+  else
+    colour = beautiful.xcolor3
+    text = count .. " updates"
+  end
+  yay_count_widget.markup = "<span foreground='" .. colour .. "'>" .. text .. "</span>"
 end
 
-local tun_status_script = [[
+local update_count_script = [[
   bash -c "
   sudo pacman -Sy && yay -Pn
   "]]
 
-awful.widget.watch(tun_status_script, update_interval, function(widget, stdout, stderr, exitreason, exitcode)
+awful.widget.watch(update_count_script, update_interval, function(widget, stdout, stderr, exitreason, exitcode)
   if exitcode == 0 then
     local count = tonumber(string.match(stdout, "%d+"))
     update_widget(count)
