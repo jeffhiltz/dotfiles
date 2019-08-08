6b6421103092c9b50dddd6848fa4f79cd490bb06
Jeff Hiltz <jeffhiltz@gmail.com>
2019-08-08 21:30:36 +0200

    simplify colors in prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -88,7 +88,7 @@ function fish_prompt --description 'Write out the prompt'
 	end
 
 	set -l arrows
-	set arrows (set_color $fish_color_cwd) "❯" (set_color $fish_color_user) "❯" (set_color $fish_color_host) "❯"
+	set arrows (set_color green) "❯" (set_color brgreen) "❯" (set_color white) "❯"
 
 	# echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "❯❯❯ "
 	echo -n -s ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" " " $arrows " "
