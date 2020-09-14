6678bd96eba3dc4d1a50bbb5ec9a2189b83038db
Jeff Hiltz <jeff.hiltz@vimcar.com>
2020-09-14 11:20:51 +0200

    add some git aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,9 @@
 set PATH ~/bin $PATH
 nvm use 12
 alias rg "command rg -S"
+
+alias gits "command git status"
+alias gitcm "command git commit -m"
+alias gita "command git add"
+alias gitd "command git diff"
+alias gitdc "command git diff --cached"
