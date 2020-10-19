749131a41dbcd9811a5c2d3756be3af32550233e
Jeff Hiltz <jeffhiltz@gmail.com>
2020-10-19 15:25:34 +0200

    add hostname when connected over ssh

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -90,6 +90,11 @@ function fish_prompt --description 'Write out the prompt'
 	set -l arrows
 	set arrows (set_color green) "❯" (set_color brgreen) "❯" (set_color white) "❯"
 
+        set -l remote_host
+        if set -q SSH_CLIENT
+                set remote_host ' ' (set_color $fish_color_host) "$__fish_prompt_hostname" "$normal"
+        end
+
 	# echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "❯❯❯ "
-	echo -n -s ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" " " $arrows " "
+	echo -n -s $remote_host ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" " " $arrows " "
 end
