e2c4de12afec2188730f5b446234d42e61f31ca1
Jeff Hiltz <jeff.hiltz@vimcar.com>
2023-02-02 10:52:28 +0100

    update script; now uses identifiers with dashes

--- bin/bin/extmon.sh
+++ bin/bin/extmon.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 960x2160 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --mode 3840x2160 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+# xrandr --output eDP-1 --mode 1920x1080 --pos 960x2160 --rotate normal --output HDMI-2 --off --output DP-2 --off --output HDMI-1 --off --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal --output VIRTUAL-1 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 960x2160 --rotate normal --output DP-1 --off --output DP-2 --off --output HDMI-1 --off --output HDMI-2 --mode 3840x2160 --pos 0x0 --rotate normal --output VIRTUAL-1 --off
